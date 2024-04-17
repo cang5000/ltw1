f7c5665cbde04ed768ff4a8acb09b1b43d7af731
cang5000 <144362135+cang5000@users.noreply.github.com>
2024-04-17 14:54:51 +0700

    up

---
+++ WebBanQuanAo/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebBanQuanAo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100)]
+        [DisplayName("Tên sản phẩm")]
+        public string? Name { get; set; }
+        [Range(0.01, 10000.00)]
+        [DisplayName("Giá")]
+        public decimal Price { get; set; }
+        [DisplayName("Mô tả")]
+        public string? Description { get; set; }
+        [DisplayName("Ảnh")]
+        public string? ImageUrl { get; set; }
+        public List<ProductImage>? Images { get; set; }
+        public int CategoryId { get; set; }
+        [DisplayName("Loại sản phẩm")]
+        public Category? Category { get; set; }
+    }
+}
---
+++ WebBanQuanAo/Program.cs
@@ -0,0 +1,61 @@
+using WebBanQuanAo.Data;
+using WebBanQuanAo.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+builder.Services.AddScoped<IProductRepository, EFProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, EFCategoryRepository>();
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddDefaultTokenProviders()
+.AddDefaultUI()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseSession();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+// ??t tr??c UseRouting
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapRazorPages();
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllerRoute(
+      name: "Admin",
+      pattern: "{area:exists}/{controller=ProductManage}/{action=Index}/{id?}"
+    );
+});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
